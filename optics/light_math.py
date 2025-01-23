642344d6c8266183acce5f29dfb320953f9aa6d4
darikoneil <darik.oneil.software@gmail.com>
2025-01-23 16:51:48 -0500

    photodiode

--- optics/light_math.py
+++ optics/light_math.py
@@ -2,6 +2,8 @@ import numpy as np
 from operator import mul
 from itertools import accumulate
 from math import sin, pi
+from collections import namedtuple
+from types import SimpleNamespace
 
 
 __all__ = []
@@ -45,22 +47,6 @@ def find_diffraction_efficiency(levels: int) -> float:
     return 100.0 * (sin(pi / levels) / (pi / levels)) ** 2
 
 
-def find_rayleigh_length(
-    wavelength: Number,
-    beam_diameter: Optional[Number] = None,
-    n: Number = 1,
-    w0: Optional[Number] = None,
-) -> float: ...
-
-
-def find_diffraction_limit(
-    wavelength: Number,
-    f: Number,
-    beam_diameter: Optional[Number] = None,
-    w0: Optional[Number] = None,
-) -> float: ...
-
-
 def complete_lens_makers_equation(): ...
 
 
@@ -77,3 +63,49 @@ def find_depth_of_field(): ...
 
 
 def find_radiant_flux(): ...
+
+
+def photodiode_battery_check(measured_voltage: float,
+                             load_resistor: int = 50,
+                             current_limiting_resistor: int = 1050,
+                             battery_voltage: float = 9.0
+                             ) -> namedtuple:
+
+    expected_v_out = find_expected_v_out(load_resistor, current_limiting_resistor, battery_voltage)
+    return (namedtuple('PhotodiodeBatteryCheck',
+                      ['expected_v_out', 'measured_v_out', 'is_battery_ok'])
+            (expected_v_out, measured_voltage, expected_v_out <= measured_voltage)
+            )
+
+
+def find_expected_v_out(load_resistor: int = 50,
+                        current_limiting_resistor: int = 1050,
+                        battery_voltage: float = 9.0) -> float:
+    return battery_voltage * load_resistor / (load_resistor + current_limiting_resistor)
+
+
+def find_bandwidth_and_response(load_resistor: int = 50,
+                                diode_capacitance: float = 1e-12) -> float:
+    return 1 / (2 * pi * load_resistor * diode_capacitance)
+
+
+def find_rise_time(load_resistor: int = 50,
+                   diode_capacitance: float = 1e-12) -> float:
+    return 0.35 / (2 * pi * load_resistor * diode_capacitance)
+
+
+def find_expected_current(peak_response: float, incident_power) -> float:
+    return peak_response * incident_power
+
+
+class Photodiode(SimpleNamespace):
+    wavelength_range: tuple[int, int] = (200e-9, 1100e-9)
+    peak_wavelength: int = 730e-9
+    peak_response: float = 0.44
+    shunt_resistance: float = 1e9
+    diode_capacitance: 1e-12
+    rise_time: 1e-9
+    max_dark_current: 2.5e-9
+    voltage_output: tuple[float, float] = (0.0, 10.0)
+    bias_voltage: float = 10.0
+    peak_response_at_920nm: float = 0.19
